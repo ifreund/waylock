fdc276e7e7af55e057be4226b43415492f7d8e5d
Isaac Freund <ifreund@ifreund.xyz>
2020-01-28 22:02:50 +0100

    Basic initialization with sctk

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,61 @@
-extern crate byteorder;
-extern crate tempfile;
-#[macro_use(event_enum)]
-extern crate wayland_client;
-extern crate wayland_protocols;
-extern crate xkbcommon;
+#[macro_use(environment)]
+extern crate smithay_client_toolkit as sctk;
 
-pub mod input;
-pub mod output;
+use sctk::environment::{Environment, SimpleGlobal};
+use sctk::output::OutputHandler;
+use sctk::reexports::calloop;
+use sctk::reexports::client::protocol::{
+    wl_compositor, wl_keyboard, wl_output, wl_seat, wl_shm, wl_surface,
+};
+use sctk::reexports::client::{Display, Proxy};
+use sctk::reexports::protocols::wlr::unstable::{
+    input_inhibitor::v1::client::zwlr_input_inhibit_manager_v1,
+    layer_shell::v1::client::zwlr_layer_shell_v1,
+};
+use sctk::seat::keyboard::{map_keyboard, Event as KbEvent, RepeatKind};
+use sctk::seat::SeatHandler;
+use sctk::shm::ShmHandler;
 
-use wayland_client::protocol::{wl_keyboard, wl_seat};
-use wayland_client::{Display, EventQueue, Filter, GlobalManager};
-use wayland_protocols::wlr::unstable::layer_shell::v1::client::zwlr_layer_surface_v1;
+struct LockEnv {
+    compositor: SimpleGlobal<wl_compositor::WlCompositor>,
+    layer_shell: SimpleGlobal<zwlr_layer_shell_v1::ZwlrLayerShellV1>,
+    inhibitor_manager: SimpleGlobal<zwlr_input_inhibit_manager_v1::ZwlrInputInhibitManagerV1>,
+    shm: ShmHandler,
+    outputs: OutputHandler,
+    seats: SeatHandler,
+}
 
-use std::cell::Cell;
-use std::cell::RefCell;
-use std::rc::Rc;
-
-use crate::input::Input;
-use crate::output::Output;
-
-event_enum!(
-    Events |
-    Keyboard => wl_keyboard::WlKeyboard,
-    LayerSurface => zwlr_layer_surface_v1::ZwlrLayerSurfaceV1
+environment!(LockEnv,
+    singles = [
+        wl_compositor::WlCompositor => compositor,
+        zwlr_layer_shell_v1::ZwlrLayerShellV1 => layer_shell,
+        zwlr_input_inhibit_manager_v1::ZwlrInputInhibitManagerV1 => inhibitor_manager,
+        wl_shm::WlShm => shm, 
+    ],
+    multis = [
+        wl_output::WlOutput => outputs,
+        wl_seat::WlSeat => seats,
+    ]
 );
 
-struct State {
-    display: Display,
-    event_queue: EventQueue,
-    input_ref: Rc<RefCell<Input>>,
-    output_ref: Rc<RefCell<Output>>,
-    locked: Rc<Cell<bool>>,
-}
-
-impl State {
-    pub fn new() -> Self {
-        // Connect to the server
-        let display = Display::connect_to_env().unwrap();
-        let mut event_queue = display.create_event_queue();
-        let attached_display = (*display).clone().attach(event_queue.get_token());
-        // GlobalManager handles the registry for us
-        let globals = GlobalManager::new(&attached_display);
-        // Ensure the server has recieved our request and sent the globals
-        event_queue.sync_roundtrip(|_, _| unreachable!()).unwrap();
-
-        // Must instantiate the seat before the set_keyboard_interactivity request on the layer
-        // surface for gaining focus to work due to a bug in wlroots
-        let input = Input::new(&globals);
-        let output = Output::new(&globals);
-
-        Self {
-            display,
-            event_queue,
-            input_ref: Rc::new(RefCell::new(input)),
-            output_ref: Rc::new(RefCell::new(output)),
-            locked: Rc::new(Cell::new(true)),
-        }
-    }
-}
-
 fn main() {
-    let mut state = State::new();
-
-    let output_ref = state.output_ref.clone();
-    let input_ref = state.input_ref.clone();
-    let locked = state.locked.clone();
-    let common_filter = Filter::new(move |event, _| match event {
-        Events::LayerSurface { event, .. } => output_ref.borrow_mut().handle_event(event, &locked),
-        Events::Keyboard { event, .. } => input_ref.borrow_mut().handle_event(event, &locked),
-    });
-
-    state
-        .output_ref
-        .borrow_mut()
-        .layer_surface
-        .assign(common_filter.clone());
-
-    let mut keyboard_created = false;
-    state
-        .input_ref
-        .borrow_mut()
-        .seat
-        .assign_mono(move |seat, event| {
-            if let wl_seat::Event::Capabilities { capabilities } = event {
-                if !keyboard_created && capabilities.contains(wl_seat::Capability::Keyboard) {
-                    // create the keyboard only once
-                    keyboard_created = true;
-                    seat.get_keyboard().assign(common_filter.clone());
-                }
-            }
-        });
-
-    while state.locked.get() {
-        state.display.flush().unwrap();
-        state
-            .event_queue
-            .dispatch(|_, _| { /* ignore unfiltered messages
-                 TODO: error on unfiltered messages */
-            })
-            .expect("Error dispatching");
-    }
+    let (lock_env, display, queue) = {
+        let display = Display::connect_to_env().unwrap();
+        let mut queue = display.create_event_queue();
+        let lock_env = Environment::init(
+            &Proxy::clone(&display).attach(queue.token()),
+            LockEnv {
+                compositor: SimpleGlobal::new(),
+                layer_shell: SimpleGlobal::new(),
+                inhibitor_manager: SimpleGlobal::new(),
+                shm: ShmHandler::new(),
+                outputs: OutputHandler::new(),
+                seats: SeatHandler::new(),
+            },
+        );
+        let ret = queue.sync_roundtrip(&mut (), |_, _, _| unreachable!());
+        ret.and_then(|_| queue.sync_roundtrip(&mut (), |_, _, _| unreachable!())).expect("Error during initial setup");
+        
+        (lock_env, display, queue)
+    };
 }
