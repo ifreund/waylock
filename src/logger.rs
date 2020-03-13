b2001b0c78e567a52ecae896862bf1f65a3009ff
Isaac Freund <ifreund@ifreund.xyz>
2020-03-13 04:03:54 +0100

    Only output color escape codes if running in tty

--- src/logger.rs
+++ src/logger.rs
@@ -6,11 +6,15 @@ use std::time::SystemTime;
 
 pub struct Logger {
     level_filter: log::LevelFilter,
+    use_color: bool,
 }
 
 impl Logger {
     fn new(level_filter: log::LevelFilter) -> Self {
-        Self { level_filter }
+        Self {
+            level_filter,
+            use_color: atty::is(atty::Stream::Stderr),
+        }
     }
 
     pub fn init(level_filter: log::LevelFilter) -> Result<(), log::SetLoggerError> {
@@ -29,19 +33,24 @@ impl log::Log for Logger {
         if self.enabled(record.metadata()) {
             let timestamp = format_rfc3339_seconds(SystemTime::now());
 
-            let level = match record.level() {
-                log::Level::Error => Color::Red.paint("ERROR"),
-                log::Level::Warn => Color::Yellow.paint("WARNING"),
-                log::Level::Info => Color::Green.paint("INFO"),
-                log::Level::Debug => Color::Blue.paint("DEBUG"),
-                log::Level::Trace => Color::Cyan.paint("TRACE"),
+            let (color, text) = match record.level() {
+                log::Level::Error => (Color::Red, "ERROR"),
+                log::Level::Warn => (Color::Yellow, "WARNING"),
+                log::Level::Info => (Color::Green, "INFO"),
+                log::Level::Debug => (Color::Blue, "DEBUG"),
+                log::Level::Trace => (Color::Cyan, "TRACE"),
             };
 
             eprintln!(
                 "[{}][{}] {}: {}",
                 timestamp,
-                record.module_path().unwrap_or("unknown"),
-                level,
+                record.module_path().unwrap_or("<unknown>"),
+                // TODO: get rid of these allocations
+                if self.use_color {
+                    color.paint(text).to_string()
+                } else {
+                    text.to_owned()
+                },
                 record.args()
             );
         }
