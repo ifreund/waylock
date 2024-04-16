5d87b940eec8c696c604d6979e8ce7ae93113349
Isaac Freund <mail@isaacfreund.com>
2024-04-16 13:43:50 +0200

    build: bump version to 1.1.0-dev

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ const Scanner = @import("deps/zig-wayland/build.zig").Scanner;
 /// development with the "-dev" suffix.
 /// When a release is tagged, the "-dev" suffix should be removed for the commit that gets tagged.
 /// Directly after the tagged commit, the version should be bumped and the "-dev" suffix added.
-const version = "1.0.0";
+const version = "1.1.0-dev";
 
 pub fn build(b: *Build) !void {
     const target = b.standardTargetOptions(.{});
